1f3b35a274f40f58632bd07fecea5068841989e6
andrewmaccormack <amaccormack@gmail.com>
2020-03-25 15:25:29 +0000

    Added expected error tests for PUT/POST/DELETE

--- test_00_products.py
+++ test_00_products.py
@@ -9,3 +9,24 @@ def test_products_at_startup():
     assert resp.status_code == 200
     resp_body = resp.json()
     assert len(resp_body) == 3
+
+def test_put_products():
+    initial_count = len(get_products().json())
+    resp=requests.put(api_url + "products", {"Name": "n/a"})
+    # Any error, maybe 405?
+    assert resp.status_code>=400
+    assert len(get_products().json()) == initial_count
+
+def test_post_products():
+    initial_count = len(get_products().json())
+    resp=requests.post(api_url + "products", {"Name": "n/a"})
+    # Any error, maybe 405?
+    assert resp.status_code>=400
+    assert len(get_products().json()) == initial_count
+
+def test_delete_products():
+    initial_count = len(get_products().json())
+    resp=requests.delete(api_url + "products")
+    # Any error, maybe 405?
+    assert resp.status_code>=400
+    assert len(get_products().json()) == initial_count
